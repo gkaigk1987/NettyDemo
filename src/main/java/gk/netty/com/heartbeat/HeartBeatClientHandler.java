1c67da5f7a34926a2290b9a2df09f0344bdd6330
GaoKai <gkaigk1987@gmail.com>
2019-02-13 13:49:12 +0800

    更新netty版本
    更新netty版本

--- src/main/java/gk/netty/com/heartbeat/HeartBeatClientHandler.java
+++ src/main/java/gk/netty/com/heartbeat/HeartBeatClientHandler.java
@@ -31,7 +31,7 @@ public class HeartBeatClientHandler extends ChannelInboundHandlerAdapter {
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
 		System.out.println("激活时间："+ new Date());
-		 System.out.println("HeartBeatClientHandler channelActive");
+		System.out.println("HeartBeatClientHandler channelActive");
 //		 ctx.fireChannelActive();
 	}
 
@@ -42,8 +42,7 @@ public class HeartBeatClientHandler extends ChannelInboundHandlerAdapter {
 	}
 
 	@Override
-	public void channelRead(ChannelHandlerContext ctx, Object msg)
-			throws Exception {
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 		String message = (String) msg;  
         System.out.println(message);  
 //        if (message.equals("HEARTBEAT")) {  
